9a29189445d990820db5a87f0577d81eb76570b2
Mal0 <shaneb@datanamix.com>
2025-07-22 10:58:01 +0200

    A tool set for personal use

--- main_test_runner.py
+++ main_test_runner.py
@@ -1,17 +1,17 @@
 import sys
 import pytest
+import os
 
 def main():
     """
     Main entry point to run all tests in the ServiceTools.tools package.
     """
-    # Run all tests in the tools/tests directory
+    # Run all tests in the current working directory
     exit_code = pytest.main([
-        "C:/Signiflow.Scripts/PythonProject/ServiceTools/tools/tests",
+         os.path.join(os.getcwd(), "tools/tests"),
         "-v"
     ])
     sys.exit(exit_code)
 
 if __name__ == "__main__":
     main()
-
